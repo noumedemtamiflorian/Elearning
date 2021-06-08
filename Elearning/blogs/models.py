7584d463f5f637b97e3381a26fa19feb60813340
Noumedem Tami Florian <noumedemtamiflorian@gmail.com>
2021-06-08 19:08:42 +0100

    Ajout de models de base avec les fixtures

--- Elearning/blogs/models.py
+++ Elearning/blogs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField(null=False)
+    category = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
